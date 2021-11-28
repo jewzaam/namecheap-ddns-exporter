794bd3a2f45cc5cca0496fb86d293bf39406925e
Naveen Malik <nmalik@redhat.com>
2021-11-28 10:59:12 -0500

    Update namecheap-ddns-exporter.py

--- src/py/namecheap-ddns-exporter.py
+++ src/py/namecheap-ddns-exporter.py
@@ -57,6 +57,6 @@ if __name__ == '__main__':
     while True:
         config = {}
         with open(args.config, 'r') as f:
-            config =  yaml.load(f, Loader=yaml.FullLoader)
+            config =  yaml.load(f)
         updateDDNS(config)
         time.sleep(config['refresh_delay_seconds'])
